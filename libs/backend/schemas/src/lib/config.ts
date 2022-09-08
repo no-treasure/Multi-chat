bbccdf4e6206bd1ed002d9536c7c8453c729bd59
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2022-09-09 00:04:24 +0300

    remove NODE_ENV

--- libs/backend/schemas/src/lib/config.ts
+++ libs/backend/schemas/src/lib/config.ts
@@ -2,8 +2,7 @@ import { Type } from "@sinclair/typebox"
 
 const ConfigSchema = Type.Object({
   DATABASE_URL: Type.String(),
-  JWT_SECRET: Type.String(),
-  NODE_ENV: Type.Union([Type.Literal("TEST"), Type.Literal("DEV"), Type.Literal("PROD")])
+  JWT_SECRET: Type.String()
 })
 
 export { ConfigSchema }
