fdf2cb3655293f5a1d11f5524c224d28ddc8e7cd
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2022-09-13 01:56:47 +0300

    add ScrollContainer

--- libs/frontend/components/src/lib/Chat/index.tsx
+++ libs/frontend/components/src/lib/Chat/index.tsx
@@ -1,14 +1,21 @@
 import { ArrowRightIcon, AttachmentIcon, PhoneIcon } from "@chakra-ui/icons"
 import { Box, Flex, Input } from "@chakra-ui/react"
+import styled from "@emotion/styled"
 import { allMessagesAtom, messageActions, selectedRoomAtom } from "@multi-chat/frontend/stores"
 import { MessageType } from "@multi-chat/shared/types"
 import { useStore } from "@nanostores/react"
 import { MessageContentType } from "@prisma/client"
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 type Props = {
   //
 }
+
+const ScrollContainer = styled(Box)`
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
 
 const Chat: React.FC<Props> = () => {
   const [inputValue, setInputValue] = useState("")
@@ -16,6 +23,8 @@ const Chat: React.FC<Props> = () => {
   const messages = useStore(allMessagesAtom)
 
   const sendMessage = () => {
+    if (inputValue === "") return
+
     const message: MessageType.Create = {
       roomId: selectedRoomId,
       contentType: MessageContentType.text,
@@ -34,18 +43,20 @@ const Chat: React.FC<Props> = () => {
 
   return (
     <Flex width="700px" h="100%" flexDir="column" margin="0 auto">
-      <Flex flexBasis="100%" flexDir="column" justify="flex-end">
-        {messages.map((message) => (
-          <Box
-            p={2}
-            key={message.id}
-            background="blue.600"
-            borderRadius="0.75rem"
-            w="max-content"
-            mb="10px"
-          >{`${message.user.username}:  ${message.contentData.text}`}</Box>
-        ))}
-      </Flex>
+      <ScrollContainer overflow="scroll">
+        <Flex flexBasis="100%" flexDir="column" justify="flex-end">
+          {messages.map((message) => (
+            <Box
+              p={2}
+              key={message.id}
+              background="blue.600"
+              borderRadius="0.75rem"
+              w="max-content"
+              mb="10px"
+            >{`${message.user.username}:  ${message.contentData.text}`}</Box>
+          ))}
+        </Flex>
+      </ScrollContainer>
 
       <Flex p={2}>
         <Flex
---
+++ libs/frontend/components/src/lib/Menu/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+type Props = {
+  //
+}
+
+const MainMenu: React.FC<Props> = () => {
+  return <div>MENU</div>
+}
+
+export { MainMenu }
